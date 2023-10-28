f63544de3bdb684462fe1b576ba30fe8bdbd3c77
Софья Микуленко <nephilim45@yandex.ru>
2023-10-28 19:38:23 +1000

    2

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,7 +1,13 @@
 import csv
 from collections import Counter
 from datetime import datetime as dt
-from pep_parse.constants import FILE_NAME, DT_FORMAT, RESULT_DIR
+
+from pathlib import Path
+
+FILE_NAME = 'status_summary_{}.csv'
+DT_FORMAT = '%Y-%m-%d_%H-%M-%S'
+BASE_DIR = Path(__file__).parent.parent
+RESULT_DIR = BASE_DIR / 'results'
 
 
 class PepParsePipeline:
